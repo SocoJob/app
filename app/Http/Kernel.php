f5b7668c284aa1a7321971a15b92dc35f58d2b31
SocoJob <136469806+SocoJob@users.noreply.github.com>
2023-09-03 12:08:57 +0200

    state

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\HttpRedirect::class,
+        
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //\App\Http\Middleware\HttpRedirect::class,
         ],
 
         'api' => [
